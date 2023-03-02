357e9bd514c4cf2eb853cf3b3337bd2e5b180022
JoshuaFowler <jfowler8912@gmail.com>
2023-03-02 17:32:35 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Code your solutions in this file
+for (let age =30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(name, event) {
+    const shoppingCart = []
+    for (let i = 0; i < names.length; i++){
+        shoppingCart.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return shoppingCart
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "Christmas"))
+
+function countDown(i = 10){
+    while( i >= 0){
+    console.log(i)
+    --i
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const names = ["Guadalupee", "Ollie", "Aki"];
+// function writeCards(names, event) {
+//   const shoppingCart = []
+  
+//     for (let i = 0; i < names.length; i++){
+//         shoppingCart.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+//     } 
+//     return shoppingCart
+// }
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "Christmas"))
